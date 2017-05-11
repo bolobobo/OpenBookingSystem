46ba70e1539445e22e7ac697f749e5ebba1d4239
bolobobo <jg4803@nyu.edu>
2017-05-11 15:10:05 -0400

    create the tag_resource file

---
+++ tag_resource.py
@@ -0,0 +1,42 @@
+'''This is the model class of resource tags.
+It is used to group several resources with the same tag.
+The data is stored in google datastore'''
+
+from google.appengine.ext import ndb
+
+class TagResource(ndb.Model):
+    '''assign each resource into specific tag group
+    parent = tag content
+    key = resource_id + parent
+    resource_id = the unique id of each resource'''
+    resource_id = ndb.GenericProperty()
+
+    @classmethod
+    def create_tag_resource(cls, resource_id, parent_key):
+        '''create a tag_resource, the key is the combination of tag and resource id'''
+        res = TagResource()
+        res.key = ndb.Key(resource_id, parent=parent_key)
+        res.resource_id = resource_id
+        res.put()
+
+    @classmethod
+    def delete_tag_resource(cls, resource_id, parent_key):
+        '''delete a entity if a resource do not belong to a tag anymore'''
+        res_key = ndb.Key(resource_id, parent=parent_key)
+        res_key.delete()
+
+    @classmethod
+    def get_resource_group(cls, ancestor_key):
+        '''the ancestor key is ndb.Key("Tags", tag), return list of tag_resource entity for given ancestor key'''
+        # todo: order by resource counts: cls.query(ancestor=ancestor_key).order(-cls.date)
+        #ancestor_key = ndb.Key('Tags', tag)
+        # return the Query object
+        return cls.query(ancestor=ancestor_key).fetch()
+
+
+
+
+    # @classmethod
+    # def query_top_tags(cls):
+    #     print cls.query().fetch()
+
